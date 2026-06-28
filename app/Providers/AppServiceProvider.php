4dfb0606f940461fc49b6209819e7338c33abf65
akrez <akrez.like@gmail.com>
2026-06-28 19:39:06 +0330

    AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Blade::directive('spaceless', function () {
+            return '<?php ob_start(); ob_implicit_flush(false); ?>';
+        });
+        Blade::directive('endspaceless', function () {
+            return "<?php echo trim(preg_replace('/>\s+</', '><', ob_get_clean())); ?>";
+        });
     }
 }
